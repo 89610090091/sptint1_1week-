0a893069851180916a9e39788c67fe849e386884
Alexander <rzn.russia4912@gmail.com>
2025-12-07 16:59:49 +0300

    typing Props 	modified:   src/App.tsx 	modified:   src/TodolistItem.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,24 @@ import './App.css'
 import { TodolistItem } from './TodolistItem'
 
 export const App = () => {
+
+  const tasks_1: Task[] = [
+    { id: 1, title: "HTML & CSS", isDone: true },
+    { id: 2, title: "JS & TS", isDone: true },
+    { id: 3, title: "REACT", isDone: false },
+    { id: 3, title: "REACT", isDone: false },
+  ]
+
+  const tasks_2: Task[] = [
+    { id: 1, title: "Beer", isDone: true },
+    { id: 2, title: "Meat", isDone: true },
+  ]
+
   return (
     <div className="app">
-      <TodolistItem />
-      <TodolistItem />
-      <TodolistItem />
+      <TodolistItem title="What to learn" tasks={tasks_1} />
+      <TodolistItem title="Songs" tasks={tasks_2} />
+      <TodolistItem title="Books" tasks={tasks_2} />
     </div>
   )
 }
--- src/TodolistItem.tsx
+++ src/TodolistItem.tsx
@@ -1,7 +1,18 @@
-export const TodolistItem = () => {
+type Props = {
+    title: string
+    tasks: Task[]
+}
+
+export type Task = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
+export const TodolistItem = (props: Props) => {
     return (
         <div>
-            <h3>What to learn</h3>
+            <h3>{props.title}</h3>
             <div>
                 <input />
                 <button>+</button>
