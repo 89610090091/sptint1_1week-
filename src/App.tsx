36915e383a0251399fb7a1164127d6e2e3af551a
Alexander <rzn.russia4912@gmail.com>
2025-12-14 00:21:29 +0300

    <Fragment></Fragment> or <></> in React? 	modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,29 +8,49 @@ export type Task = {
   isDone: boolean
 }
 
-export const App = () => {
-  const tasks1: Task[] = [
-    { id: 1, title: 'HTML&CSS', isDone: true },
-    { id: 2, title: 'JS', isDone: true },
-    { id: 3, title: 'ReactJS', isDone: false },
-    { id: 4, title: 'Redux', isDone: false },
-    { id: 5, title: 'Typescript', isDone: false },
-    { id: 6, title: 'RTK query', isDone: false },
-  ]
+// export const App = () => {
+//   const tasks1: Task[] = [
+//     { id: 1, title: 'HTML&CSS', isDone: true },
+//     { id: 2, title: 'JS', isDone: true },
+//     { id: 3, title: 'ReactJS', isDone: false },
+//     { id: 4, title: 'Redux', isDone: false },
+//     { id: 5, title: 'Typescript', isDone: false },
+//     { id: 6, title: 'RTK query', isDone: false },
+//   ]
 
-  const tasks2: Task[] = [
-    { id: 1, title: 'Hello world', isDone: true },
-    { id: 2, title: 'I am Happy', isDone: true },
-    { id: 3, title: 'Yo', isDone: false },
-  ]
+//   const tasks2: Task[] = [
+//     { id: 1, title: 'Hello world', isDone: true },
+//     { id: 2, title: 'I am Happy', isDone: true },
+//     { id: 3, title: 'Yo', isDone: false },
+//   ]
 
+//   return (
+//     <div className="app">
+//       <TodolistItem title="What to learn" tasks={tasks1} date="24.012027" />
+//       <TodolistItem title="Songs" tasks={tasks2} />
+//     </div>
+//   )
+// }
+
+/** 1 вариант */
+export const Component1 = () => {
   return (
-    <div className="app">
-      <TodolistItem title="What to learn" tasks={tasks1} date="24.012027" />
-      <TodolistItem title="Songs" tasks={tasks2} />
-    </div>
+    <>
+      <TodolistItem />
+      <TodolistItem />
+      <TodolistItem />
+    </>
   )
 }
-
+/** 2 вариант */
+export const Component2 = () => {
+  return (
+    <Fragment>
+      <TodolistItem />
+      <TodolistItem />
+      <TodolistItem />
+    </Fragment>
+  )
+}
 
 
